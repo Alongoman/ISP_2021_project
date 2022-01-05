7ead7469483e80c04bfda9cafb63866afe8e693d
Alongoman <alon.goldmann@gmail.com>
2022-01-05 09:56:10 +0200

    funcs

---
+++ Hackathon/functions/dip_rgb2hsv.m
@@ -0,0 +1,31 @@
+% DIP - Alon Goldmann 312592173, Yogev Hadadi 311436273
+
+function [H,S,V] = dip_rgb2hsv(img) % get a N x M x 3 matrix
+    img = norm_pic(img);
+    
+    R = img(:,:,1);
+    G = img(:,:,2);
+    B = img(:,:,3);
+    c_max = max(R,G);
+    c_max = max(c_max,B);
+    c_min = min(R,G);
+    c_min = min(c_min,B);
+    d = c_max-c_min;
+    
+    V = c_max;
+    S = 0*(c_max == 0) + (d./c_max).*(c_max ~= 0);
+    
+    H = zeros(size(R));
+    temp=mod(((G-B)./d),6);
+    H(c_max==R)=temp(c_max==R);
+
+    temp=(B-R)./d +2;
+    H(c_max==G)=temp(c_max==G);
+
+    temp=(R-G)./d +4;
+    H(c_max==B)=temp(c_max==B);
+
+    H(d==0)=0;
+
+    H=H/6;
+    end
--- Hackathon/seg6.m
+++ Hackathon/seg6.m
@@ -1,30 +1,40 @@
 
-function [rec_params,BW] = seg6(img)
-
-if(size(img,3)>1)
-    img = rgb2gray(img);
-end
-
+function [rec_params,BB] = seg6(img)
+[n,m,k] = size(img);
 rec_params = zeros([1,4]);
-BW = edge(img);
+BW = zeros(n,m);
 
-%create the polyshape
-[poly_x,poly_y] = find(BW==1);
-polyin = polyshape(poly_x,poly_y);
-[ylim,xlim] = boundingbox(polyin);
+if k < 3
+   BW = ones(n,m);
+else
 
-if(length(ylim)<2 || length(xlim)<2)
-    return;
+
+[c,m,y,k] = dip_rgb2cymk(
+
+[idx,C] = kmeans(img2,5);
+img3 = reshape(idx,n,m);
+figure; imshow(img3,[]);
+a=0;
+
+
 end
-
-%rectangular params
-dx = xlim(2) - xlim(1);
-dy = ylim(2) - ylim(1);
-
-rec_params(1) = xlim(1);
-rec_params(2) = ylim(1);
-rec_params(3) = dx;
-rec_params(4) = dy;
+% %create the polyshape
+% [poly_x,poly_y] = find(BW==1);
+% polyin = polyshape(poly_x,poly_y);
+% [ylim,xlim] = boundingbox(polyin);
+% 
+% if(length(ylim)<2 || length(xlim)<2)
+%     return;
+% end
+% 
+% %rectangular params
+% dx = xlim(2) - xlim(1);
+% dy = ylim(2) - ylim(1);
+% 
+% rec_params(1) = xlim(1);
+% rec_params(2) = ylim(1);
+% rec_params(3) = dx;
+% rec_params(4) = dy;
 
 rec_params = uint16(rec_params);
 BB = logical(BW);
