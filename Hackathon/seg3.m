c17a57e68f456c01f0ea957553d2ec6c2ce90818
yogevhad <yogevhad987@gmail.com>
2022-01-05 11:12:35 +0200

    seg3 work

--- Hackathon/seg3.m
+++ Hackathon/seg3.m
@@ -1,35 +1,55 @@
-function [BB, mask] = seg3(img)
-
+function [BB,mask]=seg3(img1)
+    img1=double(img1);
+    img1=(img1-min(img1(:)))./(max(img1(:)-min(img1(:))));
+    [h,s,v]=rgb2hsv(img1);
+    Lab=rgb2lab(img1);
+    a_img1=Lab(:,:,2);
+    b_img1=Lab(:,:,3);
     
-    %if(size(img,3)>1)
-    %    img = rgb2gray(img);
-    %end
+    mask=(s>0.6);
+    mask = medfilt2(mask);
+    mask = medfilt2(mask);
+    mask = medfilt2(mask,[5 5]);
+    mask = medfilt2(mask,[5 5]);
+    se90=strel('line',3,90);
+    se0=strel('line',3,0);
+    mask=imdilate(mask,[se90 se0]);
+    mask=imfill(mask,'holes');
+    se90=strel('line',3,90);
+    se0=strel('line',3,0);
+    mask=imdilate(mask,[se90 se0]);
+    mask=imfill(mask,'holes');
+    se90=strel('line',3,90);
+    se0=strel('line',3,0);
+    mask=imdilate(mask,[se90 se0]);
+    mask=imfill(mask,'holes');
+    se90=strel('line',3,90);
+    se0=strel('line',3,0);
+    mask=imdilate(mask,[se90 se0]);
+    mask=imfill(mask,'holes');
+    se90=strel('line',3,90);
+    se0=strel('line',3,0);
+    mask=imdilate(mask,[se90 se0]);
+    mask=imfill(mask,'holes');
 
-    BB = zeros([1,4]);
- if(size(img,3)<3)
-        mask = ones(size(img));
-    else
-         mask = edge(img(:,:,2),'Canny');
-        mask = bwareafilt(mask,1);
- end
-    
     [poly_x,poly_y] = find(mask==1);
     polyin = polyshape(poly_x,poly_y);
     [ylim,xlim] = boundingbox(polyin);
-
+    
     if(length(ylim)<2 || length(xlim)<2)
         return;
     end
-
+    
     %rectangular params
     dx = xlim(2) - xlim(1);
     dy = ylim(2) - ylim(1);
-
-    BB(1) = xlim(1);
-    BB(2) = ylim(1);
-    BB(3) = dx;
-    BB(4) = dy;
-
-    BB = uint16(BB);
+    
+    rec_params(1) = xlim(1);
+    rec_params(2) = ylim(1);
+    rec_params(3) = dx;
+    rec_params(4) = dy;
+    
+    BB = uint16(rec_params);
     mask = logical(mask);
 end
+
