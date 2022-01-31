7f6703937ed8e5450971412ddf6634fc577a2f39
Ami Berger <54857763+amiberger2@users.noreply.github.com>
2022-01-31 19:17:21 +0200

    2d_gaussians

--- new_gui/initial_hand_rep_hs.m
+++ new_gui/initial_hand_rep_hs.m
@@ -7,7 +7,9 @@ function [handles,first_time_hand_BB] = initial_hand_rep_hs(handles,img,points)
 %   a lot to know if rthe original hand that was found is good, so using this
 %   parameter we can mark the location where the hand was found. 
 
-    err=1.95;  % the distance we accept from the mean, which is where the
+    err=2;
+    err1=4;
+    % the distance we accept from the mean, which is where the
 %   value is of size mean*exp(-err) the bigger it is the more the
 %   segmentation will include
     point = floor(points(2,:));
@@ -30,8 +32,8 @@ function [handles,first_time_hand_BB] = initial_hand_rep_hs(handles,img,points)
     % light is too strong or too weak the sturation and hue are not stable.  
     tmp=sat_small; %so we ignore those areas.
 
-    x = tmp(value_mask);
-    x_point=sat_small(point(2),point(1));
+    x = [hue_small(value_mask) sat_small(value_mask)];
+    x_point=[hue_small(point(2),point(1));sat_small(point(2),point(1))];
     
 
     AIC = zeros(1,4);
@@ -45,22 +47,26 @@ function [handles,first_time_hand_BB] = initial_hand_rep_hs(handles,img,points)
     [~,numComponents] = min(AIC);
     gmodel = GMModels{numComponents};
 
-    Sigma=zeros(1,numComponents);
-    Mu=zeros(1,numComponents);
+    Sigma=zeros(2,numComponents);
+    Mu=zeros(2,numComponents);
     dist_vec=zeros(1,numComponents);
     sigma=gmodel.Sigma;
     for k=1:numComponents
-        Sigma(k)=sigma(:,:,k);
-        Mu(k)=gmodel.mu(k);
-        dist_vec(k)=abs(x_point-Mu(k));
+        Sigma(:,k)=sigma(:,:,k);
+        Mu(:,k)=gmodel.mu(k,:)';
+        dist_vec(k)=norm(x_point-Mu(:,k),2);
     end
     [~,hand_k]=min(dist_vec);
-    miu_h=Mu(hand_k);sigma_h=Sigma(hand_k);
+    miu_h=Mu(:,hand_k);sigma_h=Sigma(:,hand_k);
+
     
-    hand.sat_low_th = miu_h-sqrt(2*sigma_h*(err-0.5*log(2*pi*sigma_h)));
-    hand.sat_high_th = miu_h+sqrt(2*sigma_h*(err-0.5*log(2*pi*sigma_h)));
-    
-    mask=logical(value_mask.*(tmp>=hand.sat_low_th).*(tmp<=hand.sat_high_th));
+    hand.sat_low_th = miu_h(2)-sqrt(2*sigma_h(2)*(err1-0.5*log(2*pi*sigma_h(2))));
+    hand.sat_high_th = miu_h(2)+sqrt(2*sigma_h(2)*(err1-0.5*log(2*pi*sigma_h(2))));
+    hand.hue_low_th = miu_h(1)-sqrt(2*sigma_h(1)*(err1-0.5*log(2*pi*sigma_h(1))));
+    hand.hue_high_th = miu_h(1)+sqrt(2*sigma_h(1)*(err1-0.5*log(2*pi*sigma_h(1))));
+
+    mask=logical(value_mask.*(sat_small>=hand.sat_low_th).*(sat_small<=hand.sat_high_th));
+    mask=logical(mask.*(hue_small>=hand.hue_low_th).*(hue_small<=hand.hue_high_th));
 
     
     % Hue
@@ -68,28 +74,17 @@ function [handles,first_time_hand_BB] = initial_hand_rep_hs(handles,img,points)
     x = tmp(mask);%here we take the hue that are part of sat group (the hue of object with high sat)
     x_point=hue_small(point(2),point(1));
 
-    AIC = zeros(1,5);
-    GMModels = cell(1,5);
-    options = statset('MaxIter',500);
-    for k = 2:5
-      GMModels{k} = fitgmdist(x,k,'Options',options,'CovarianceType','diagonal');
-      AIC(k)= GMModels{k}.AIC;
+    gmodel=fitgmdist(x,2);
+    Sigma=gmodel.Sigma;
+    sigma_h1=Sigma(:,:,1);
+    sigma_h2=Sigma(:,:,2);
+    miu_h1=gmodel.mu(1);
+    miu_h2=gmodel.mu(2);
+    if abs(x_point-miu_h1)<=abs(x_point-miu_h2)
+        miu_h=miu_h1;sigma_h=sigma_h1;
+    else
+        miu_h=miu_h2;sigma_h=sigma_h2;
     end
-
-    [~,numComponents] = min(AIC);
-    gmodel = GMModels{numComponents};
-
-    Sigma=zeros(1,numComponents);
-    Mu=zeros(1,numComponents);
-    dist_vec=zeros(1,numComponents);
-    sigma=gmodel.Sigma;
-    for k=1:numComponents
-        Sigma(k)=sigma(:,:,k);
-        Mu(k)=gmodel.mu(k);
-        dist_vec(k)=abs(x_point-Mu(k));
-    end
-    [~,hand_k]=min(dist_vec);
-    miu_h=Mu(hand_k);sigma_h=Sigma(hand_k);
     hand.hue_low_th = miu_h-sqrt(2*sigma_h*(err-0.5*log(2*pi*sigma_h)));
     hand.hue_high_th = miu_h+sqrt(2*sigma_h*(err-0.5*log(2*pi*sigma_h)));
     
@@ -105,7 +100,7 @@ function [handles,first_time_hand_BB] = initial_hand_rep_hs(handles,img,points)
     sigma_h2=Sigma(:,:,2);
     miu_h1=gmodel.mu(1);
     miu_h2=gmodel.mu(2);
-    if normpdf(x_point,miu_h1,sigma_h1)>=normpdf(x_point,miu_h2,sigma_h2)
+    if abs(x_point-miu_h1)<=abs(x_point-miu_h2)
         miu_h=miu_h1;sigma_h=sigma_h1;
     else
         miu_h=miu_h2;sigma_h=sigma_h2;
@@ -118,6 +113,7 @@ function [handles,first_time_hand_BB] = initial_hand_rep_hs(handles,img,points)
     handles.hand = hand;
 
     tmpmask=logical((sat_small>=hand.sat_low_th).*(sat_small<=hand.sat_high_th).*(hue_small>=hand.hue_low_th).*(hue_small<=hand.hue_high_th));
+   
     se90=strel('line',3,90);se0=strel('line',3,0);%changed
     tmpmask=imdilate(tmpmask,[se90 se0]);%changed
     tmpmask=bwareafilt(tmpmask,1,"largest");
