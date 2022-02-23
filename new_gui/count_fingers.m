82d947e320f1236bcb59a8269d8b54ff1dd08421
yogevhad <yogevhad987@gmail.com>
2022-02-23 18:39:56 +0200

    Update count_fingers.m

--- new_gui/count_fingers.m
+++ new_gui/count_fingers.m
@@ -8,6 +8,8 @@
 
 function [counts, center_of_mass] = count_fingers(handles)
 img = handles.hand.mask;
+% harsh = handles.harsh;
+harsh = 1;
 palm_center = handles.hand.palm_center;
 % fist = handles.hand.fist;
 
@@ -59,33 +61,40 @@ r_orig = max(radii);
 r = ceil(r_orig/2)*r_scale;
 
 I = zeros(size(bw));
+
 elipse1 = get_elipse(r,[1.4,0.9], [x_cent,y_cent]);
-elipse2 = get_elipse(r,[1.42,1.1], [x_cent,y_cent]);
-elipse3 = get_elipse(r,[1.2,0.9], [x_cent,y_cent]);
-elipse4 = get_elipse(r,[1.2,1.1], [x_cent,y_cent]);
-
-
 A1 = imbinarize(rgb2gray(insertShape(I,'Polygon',elipse1)));
-A2 = imbinarize(rgb2gray(insertShape(I,'Polygon',elipse2)));
-A3 = imbinarize(rgb2gray(insertShape(I,'Polygon',elipse3)));
-A4 = imbinarize(rgb2gray(insertShape(I,'Polygon',elipse4)));
-
-
-%A = rgb2gray(insertShape(I,'circle',[x_cent,y_cent,r],'LineWidth',1));
-
 intersect1 = A1.*bw;
-intersect2 = A2.*bw;
-intersect3 = A3.*bw;
-intersect4 = A4.*bw;
-cell_inter={intersect1,intersect2,intersect3,intersect4};
-counts=[];
-%% counts = ceil((sum(intersect,'all'))/2);
-for i=1:4
-    [~,counts1] = bwlabel(cell_inter{i});
-    counts =[counts (ceil(counts1/2) - 1)];
-end
-counts=max(counts);
+[~,counts1] = bwlabel(intersect1);
+counts = ceil(counts1/2) - 1;
 
+if harsh
+    elipse2 = get_elipse(r,[1.42,1.1], [x_cent,y_cent]);
+    elipse3 = get_elipse(r,[1.2,0.9], [x_cent,y_cent]);
+    elipse4 = get_elipse(r,[1.2,1.1], [x_cent,y_cent]);
+    elipse5 = get_elipse(r,[1,0.9], [x_cent,y_cent]);
+    elipse6 = get_elipse(r,[1,1.1], [x_cent,y_cent]);
+    
+    A2 = imbinarize(rgb2gray(insertShape(I,'Polygon',elipse2)));
+    A3 = imbinarize(rgb2gray(insertShape(I,'Polygon',elipse3)));
+    A4 = imbinarize(rgb2gray(insertShape(I,'Polygon',elipse4)));
+    A5 = imbinarize(rgb2gray(insertShape(I,'Polygon',elipse5)));
+    A6 = imbinarize(rgb2gray(insertShape(I,'Polygon',elipse6)));
+    %A = rgb2gray(insertShape(I,'circle',[x_cent,y_cent,r],'LineWidth',1));
+    
+    intersect2 = A2.*bw;
+    intersect3 = A3.*bw;
+    intersect4 = A4.*bw;
+    intersect5 = A5.*bw;
+    intersect6 = A6.*bw;
+    cell_inter={intersect2,intersect3,intersect4, intersect5, intersect6};
+    % counts = ceil((sum(intersect,'all'))/2);
+    for i=1:length(cell_inter)
+        [~,counts1] = bwlabel(cell_inter{i});
+        counts = [counts (ceil(counts1/2) - 1)];
+    end
+    counts=max(counts);
+end
 
 if(counts<3)
 %fill_ratio = sum(img,"all")/(r_orig^2);
